250c553944c1b17f4a4fc6f8aae15b0790378c47
Yaqub naqeb <yaqub.009448401@gmail.com>
2022-09-22 16:16:30 +0300

    adding link to contact page

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -17,10 +17,10 @@ const Contacts = () => {
       </div>
       {/* Socials */}
       <div >
-        <a href="/" className='text-[#9C9C9C]'><span className='hover:text-[#000000a9] transition-all duration-300'>Facebook</span> | </a>
-        <a href="/" className='text-[#9C9C9C]'><span className='hover:text-[#000000a9] transition-all duration-300'>Instagram</span> | </a>
-        <a href="/" className='text-[#9C9C9C]'><span className='hover:text-[#000000a9] transition-all duration-300'>LinkedIn</span> | </a>
-        <a href="/" className='text-[#9C9C9C]'><span className='hover:text-[#000000a9] transition-all duration-300'>GitHub</span> </a>
+        <a target={'_blank'} rel="noreferrer" href="https://www.facebook.com/yaqubEng"  className='text-[#9C9C9C]'><span className='hover:text-[#000000a9] transition-all duration-300'>Facebook</span> | </a>
+        <a target={'_blank'} rel="noreferrer" href="https://www.instagram.com/yaqub_321_/" className='text-[#9C9C9C]'><span className='hover:text-[#000000a9] transition-all duration-300'>Instagram</span> | </a>
+        <a target={'_blank'} rel="noreferrer" href="https://www.linkedin.com/in/yaqub-naqeeb-b9894b238/" className='text-[#9C9C9C]'><span className='hover:text-[#000000a9] transition-all duration-300'>LinkedIn</span> | </a>
+        <a href="https://github.com/Yaqub-naqeb" target={'_blank'} rel="noreferrer" className='text-[#9C9C9C]'><span className='hover:text-[#000000a9] transition-all duration-300'>GitHub</span> </a>
       
         
       </div>
