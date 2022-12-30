ab6d73ff0e0857d55da16ce2747d7a1adf8d9686
Yaqub naqeb <yaqub.009448401@gmail.com>
2022-12-30 18:44:38 +0300

    ruse code

--- src/components/My.js
+++ src/components/My.js
@@ -7,7 +7,7 @@ const My = ({bg_name,name}) => {
        <div className='mb-[2rem]'>
         <p className='text-center
         lg:text-[8rem] md:text-[8rem] text-8xl  font-semibold opacity-[0.6]  text-[#ADD6E84F]'>{bg_name}</p>
-     <p className='text-[2.4rem] text-[#ADD6E8] lg:translate-y-[-5.5rem] md:translate-y-[-5.5rem]
+     <p className='text-[2.4rem] text-[#ADD6E8] lg:translate-y-[-4rem] md:translate-y-[-4rem]
      translate-y-[-3rem]
      text-center'>{name}</p>
      </div>
