a3046a04b3f605dfc0b9edb64fc2974ddac4ad75
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-01-01 18:07:39 +0300

    adding skill

--- src/components/SkillInfo.js
+++ src/components/SkillInfo.js
@@ -9,37 +9,48 @@ export const SkillInfo=[{
     img: require('./imgs/skills/CSS3_logo.svg.png')},
     {
         id:'3',
-        name:'JavaScript',
-        img: require('./imgs/skills/Unofficial_JavaScript_logo_2.svg.png')
-    },
-    {
-        id:'4',
-        name:'React',
-        img: require('./imgs/skills/react.png')
-    },
-    {
-        id:'5',
         name:'TailwindCss',
         img: require('./imgs/skills/Tailwind_CSS_Logo.svg.png')
     },
     {
+        id:'4',
+        name:'JavaScript',
+        img: require('./imgs/skills/Unofficial_JavaScript_logo_2.svg.png')
+    },
+    {
+        id:'5',
+        name:'React',
+        img: require('./imgs/skills/react.png')
+    },
+    {
         id:'6',
+        name:'NextJs',
+        img: require('./imgs/skills/next.png')
+    },
+    {
+        id:'7',
+        name:'Redux',
+        img: require('./imgs/skills/redux.png')
+    },
+    
+    {
+        id:'8',
         name:'Git',
         img: require('./imgs/skills/Git_icon.svg.png')
     },
     {
-        id:'7',
+        id:'9',
         name:'GitHub',
         img: require('./imgs/skills/Octicons-mark-github.svg.png')
     },
     {
-        id:'8',
+        id:'10',
         name:'Figma',
         img: require('./imgs/skills/figma.png')
     },
    
     {
-        id:'9',
+        id:'11',
         name:'Firebase',
         img: require('./imgs/skills/68747470733a2f2f696d672e69636f6e73382e636f6d2f636f6c6f722f3438302f66697265626173652e706e67.png')
     },
