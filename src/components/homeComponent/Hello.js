090093048bd18ec0411f9a67f121ecec46ac46d9
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-02-18 16:57:59 +0300

    make more component

---
+++ src/components/homeComponent/Hello.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import useTheme from '../store/useTheme';
+
+const Hello = () => {
+    const {mode}=useTheme();
+
+  return (
+    <div className='writting '>
+      {/* Writting */}
+      <div className='flex flex-col  items-center relative font-normal'>
+          <motion.p  initial={{ y:-100 }}
+          animate={{ y:0 }}
+          transition={{
+          duration:.6}}
+
+         className={`font-semibold  opacity-[0.6]  tracking-[4px]  ${mode?'text-[#add6e8d2]':'text-[#333]'}  lg:text-[10rem] md:text-[10rem] text-[7.5rem] hello`}>Hello</motion.p>
+          <motion.p 
+             initial={{ x:-150 }}
+             animate={{ x:0 }}
+             transition={{
+             duration:1
+             }} className={` absolute  bottom-1/4 text-[2.5rem]  imyaqub  ${mode?'text-[#83c3de]':'text-[#ADD6E8]'} `}>I'm Yaqub</motion.p>
+
+          <motion.p className='text-[#9C9C9C] text-[1.5rem]  translate-y-[-2.5rem]'>Front-end Developer</motion.p>
+           
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default Hello
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import useTheme from '../components/store/useTheme'
-import { motion } from 'framer-motion'
 import Rectangular from '../components/homeComponent/Rectangular'
 import ToggleMode from '../components/homeComponent/ToggleMode'
+import Hello from '../components/homeComponent/Hello'
 
 
 const Home = () => {
@@ -12,7 +12,7 @@ const Home = () => {
   return (
  
  <div  className={`h-[100vh]  ${mode?'white ':'dark'}
- w-[100vw] overflow-hidden   ${mode?'':'bg-[#262626]'}`}>
+ w-[100vw] overflow-hidden flex flex-col items-start justify-center  ${mode?'':'bg-[#262626]'}`}>
 
 
 {/* the blue rectaingul */}
@@ -24,32 +24,10 @@ const Home = () => {
 <ToggleMode/>
         </div>
 {/* Writting */}
-        <div className='writting  absolute top-[54%] lg:top-[28%] md:top-[60%] xl:left-[5%] lg:left-[10%] md:left-[23%] left-10  '>
-          <motion.p 
-          
-          initial={{ y:-100 }}
-          animate={{ y:0 }}
-          transition={{
-          duration:.6
-          }}
-          // #505C62
-          // 424A4E
-          className={`font-semibold opacity-[0.6] tracking-[4px]    ${mode?'text-[#add6e8d2]':'text-[#333]'}  lg:text-[10rem] md:text-[10rem] text-[7.5rem] hello`}>Hello</motion.p>
-          <motion.p 
-             initial={{ x:-150 }}
-             animate={{ x:0 }}
-             transition={{
-             duration:1
-             }}
-          
-          // #ADD6E8
-          className={`   absolute top-[58%] text-[2.5rem] left-[20%] imyaqub  ${mode?'text-[#83c3de]':'text-[#ADD6E8]'} `}>I'm Yaqub</motion.p>
-          <motion.p 
-          
-          
-          className='text-[#9C9C9C] text-[1.5rem] absolute lg:bottom-2 md:bottom-3 bottom-[-1rem] lg:left-[13%] md:left-[14%] left-[13%]'>Front-end Developer</motion.p>
-           
-        </div>
+    
+      <div className='lg:-translate-y-10 md:translate-y-72 lg:px-20  lg:self-start md:self-center self-center justify-self-end translate-y-28  '>
+      <Hello/>
+      </div>
 {/* rectangul with socials */}
           <Rectangular/>
    </div>
