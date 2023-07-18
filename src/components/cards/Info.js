e4b7080d3ccb7b9e747afa92d143ea2ea4f834cd
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-07-19 01:18:59 +0300

    my new project added

--- src/components/cards/Info.js
+++ src/components/cards/Info.js
@@ -1,104 +1,116 @@
-export const Info=[{
-    id:'1',
-    title:'LinkPlus',
-    img: require('../imgs/prjs/Screenshot (12).png'),
-    num:'01',
-    style:'',
-    tools:[
-     {tool:'NextJs'},
-     {tool:'React'},
-    //  {tool:'firebase'},
-    //  {tool:'Tailwind'},
-    //  {tool:'figma'},
-     {tool:'ReduxToolkit'},
+export const Info = [
+  {
+    id: "1",
+    title: "ErbilianWay",
+    img: require("../imgs/prjs/erblianway.png"),
+    num: "01",
+    style: "",
+    tools: [{ tool: "Laravel" }, { tool: "Bootsrap" }, { tool: "Vue" }],
+    url: "https://github.com/Yaqub-naqeb/LinkPlus",
+    demo: "https://erbilianway.zhmara.com/public/",
+    description: [
+      {
+        p: "LinkPlus is a social media platform designed to connect professionals and businesses helping them to network and find job opportunities",
+      },
+      // {p:' Users can create a profile that highlights their professional '},
+      // {p:' experience, education,and skills, and connect with other professionals'},
     ],
-url:'https://github.com/Yaqub-naqeb/LinkPlus',
-demo:'https://link-plus.vercel.app/',
-description:[
-    {p:'LinkPlus is a social media platform designed to connect professionals and businesses helping them to network and find job opportunities'},
-// {p:' Users can create a profile that highlights their professional '},
-// {p:' experience, education,and skills, and connect with other professionals'},
-]
-},
-{
-    id:'2',
-    title:'AVIBOOKS',
-    img: require('../imgs/prjs/Screenshot (39).png'),
-    num:'02',
-    style:'',
-    tools:[
-     {tool:'React'},
-     {tool:'ContextAPI'},
-     {tool:'Tailwind'},
+  },
+  {
+    id: "2",
+    title: "LinkPlus",
+    img: require("../imgs/prjs/Screenshot (12).png"),
+    num: "02",
+    style: "flex-row-reverse",
+    tools: [
+      { tool: "NextJs" },
+      { tool: "React" },
+      //  {tool:'firebase'},
+      //  {tool:'Tailwind'},
+      //  {tool:'figma'},
+      { tool: "ReduxToolkit" },
     ],
-url:'https://github.com/Yaqub-naqeb/AviBooks',
-demo:'https://avibooks.vercel.app/',
-description:[
-   
-    {p:'Avibooks is a website that you can find your books and buy it an easy way with some free books '},
-]
-},
-{
-    id:'3',
-    title:'Online Therapist',
-    img: require('../imgs/prjs/Screenshot 2023-05-02 210331.png'),
-    num:'03',
-    style:'flex-row-reverse hid',
-    tools:[
-     {tool:'Laravel'},
-     {tool:'alpine js'},
-     {tool:'MySql'},
+    url: "https://github.com/Yaqub-naqeb/LinkPlus",
+    demo: "https://link-plus.vercel.app/",
+    description: [
+      {
+        p: "LinkPlus is a social media platform designed to connect professionals and businesses helping them to network and find job opportunities",
+      },
+      // {p:' Users can create a profile that highlights their professional '},
+      // {p:' experience, education,and skills, and connect with other professionals'},
     ],
-url:'https://github.com/Yaqub-naqeb/-online-therapist',
-demo:'https://github.com/Yaqub-naqeb/-online-therapist',
-description:[
-   
-    {p:'I developed a user-friendly website that helps people schedule doctor appointments and share content. Users can easily create an account, log in, and access their personalized profile to enjoy these features.'},
-    //  My website enhances the experience of both patients and medical professionals, providing them with a simple and convenient way to book appointments and connect with others in the healthcare industry.
-]
-},
+  },
+  {
+    id: "3",
+    title: "AVIBOOKS",
+    img: require("../imgs/prjs/Screenshot (39).png"),
+    num: "03",
+    style: "",
+    tools: [{ tool: "React" }, { tool: "ContextAPI" }, { tool: "Tailwind" }],
+    url: "https://github.com/Yaqub-naqeb/AviBooks",
+    demo: "https://avibooks.vercel.app/",
+    description: [
+      {
+        p: "Avibooks is a website that you can find your books and buy it an easy way with some free books ",
+      },
+    ],
+  },
+  {
+    id: "4",
+    title: "Online Therapist",
+    img: require("../imgs/prjs/Screenshot 2023-05-02 210331.png"),
+    num: "04",
+    style: "flex-row-reverse hid",
+    tools: [{ tool: "Laravel" }, { tool: "alpine js" }, { tool: "MySql" }],
+    url: "https://github.com/Yaqub-naqeb/-online-therapist",
+    demo: "https://github.com/Yaqub-naqeb/-online-therapist",
+    description: [
+      {
+        p: "I developed a user-friendly website that helps people schedule doctor appointments and share content. Users can easily create an account, log in, and access their personalized profile to enjoy these features.",
+      },
+      //  My website enhances the experience of both patients and medical professionals, providing them with a simple and convenient way to book appointments and connect with others in the healthcare industry.
+    ],
+  },
 
-// {
-//     id:'3',
-//     demo:'https://cinflex.vercel.app/',
-//     title:'CINFLIX',
-//     img: require('../imgs/prjs/Screenshot (41).png'),
-//     num:'03',
-//     style:'',
-//     tools:[
-//      {tool:'React'},
-//      {tool:'firebase'},
-//     //  {tool:'JavaScript'},
-//      {tool:'XD'},
-//     ],
-// url:'https://github.com/Abdulbariii/Movie-info',
-// description:[
-//     {p:'Cinflix is a website that you can find your movies '},
-// {p:'and  watch the trailer of movies or watch it'}
-// ]
-// }
-// ,
-{
-    id:'4',
-    demo:'https://headset23.netlify.app/html/',
-    title:'HEADSET',
-    img: require('../imgs/prjs/Screenshot (40).png'),
-    num:'04',
-    style:'flex-row-reverse',
-    url:'https://github.com/Yaqub-naqeb/Headset',
-    tools:[
-     {tool:'Html'},
-     {tool:'Css'},
-     {tool:'JavaScript'},
-     {tool:'Figma'},
-    
+  // {
+  //     id:'3',
+  //     demo:'https://cinflex.vercel.app/',
+  //     title:'CINFLIX',
+  //     img: require('../imgs/prjs/Screenshot (41).png'),
+  //     num:'03',
+  //     style:'',
+  //     tools:[
+  //      {tool:'React'},
+  //      {tool:'firebase'},
+  //     //  {tool:'JavaScript'},
+  //      {tool:'XD'},
+  //     ],
+  // url:'https://github.com/Abdulbariii/Movie-info',
+  // description:[
+  //     {p:'Cinflix is a website that you can find your movies '},
+  // {p:'and  watch the trailer of movies or watch it'}
+  // ]
+  // }
+  // ,
+  {
+    id: "5",
+    demo: "https://headset23.netlify.app/html/",
+    title: "HEADSET",
+    img: require("../imgs/prjs/Screenshot (40).png"),
+    num: "05",
+    style: "",
+    url: "https://github.com/Yaqub-naqeb/Headset",
+    tools: [
+      { tool: "Html" },
+      { tool: "Css" },
+      { tool: "JavaScript" },
+      { tool: "Figma" },
     ],
 
-description:[
-    {p:'This website is all about selling different types of headsets when ever you need a headset for professional or personal use this website has something for you! '},]
-}
-
-
-
-]
-
+    description: [
+      {
+        p: "This website is all about selling different types of headsets when ever you need a headset for professional or personal use this website has something for you! ",
+      },
+    ],
+  },
+];
