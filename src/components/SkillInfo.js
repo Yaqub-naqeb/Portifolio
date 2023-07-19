17d6775964610181409bc9d821c16ded64b1b25e
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-07-19 18:40:43 +0300

    add some skills

--- src/components/SkillInfo.js
+++ src/components/SkillInfo.js
@@ -1,15 +1,16 @@
 import html from './svg/html.svg'
 import css from './svg/css.svg'
-import scss from './svg/sasss.svg'
+import sass from './imgs/skills/5968550.png'
 import javaScript from './svg/js.svg'
 import react from './svg/reactsvg.svg'
-import nextjs from './svg/Nextjs.svg'
 import redux from './svg/redaaxxx.svg'
 import git from './svg/gitfinal.svg'
+import python from './imgs/skills/python (1).png'
 import github from './svg/github.svg'
-import firebase from './svg/newfirebase.svg'
 import tailwind from './svg/tailwind.svg'
-
+import bootsrap from './imgs/skills/bootstrap.png'
+import sql from './imgs/skills/sql-server.png'
+import mysql from './imgs/skills/mysql.png';
 
 
 export const SkillInfo=[{
@@ -19,55 +20,99 @@ export const SkillInfo=[{
     img: html
 
 },
+
+
+
 {
     id:'2',
     name:'Css',
     img: css},
+
+
+{
+    id:'3',
+    name:'Bootsrap',
+    img: bootsrap},
+
+
+
+
+ 
+{
+    id:'4',
+    name:'Sass',
+    img: sass},
     {
-        id:'3',
+        id:'5',
         name:'TailwindCss',
         img: tailwind
     },
+
+
     {
-        id:'4',
+        id:'6',
         name:'JavaScript',
         img: javaScript
     },
     {
-        id:'5',
+        id:'7',
+        name:'Python',
+        img: python
+    },
+
+
+
+
+
+
+
+
+
+    {
+        id:'8',
         name:'React',
         img: react
     },
     {
-        id:'6',
+        id:'9',
         name:'NextJs',
         img: require('./imgs/skills/next.png')
     },
     {
-        id:'7',
+        id:'10',
         name:'Redux',
         img:redux
     },
     
     {
-        id:'8',
+        id:'11',
         name:'Git',
         img: git
     },
     {
-        id:'9',
+        id:'12',
         name:'GitHub',
         img: github
     },
     {
-        id:'10',
+        id:'13',
         name:'Figma',
         img: require('./imgs/skills/blackofigma.png')
     },
    
     {
-        id:'11',
+        id:'14',
         name:'Firebase',
         img: require('./imgs/skills/fb-removebg-preview.png')
+    },
+    {
+        id:'15',
+        name:'Sql',
+        img:sql
+    },
+    {
+        id:'16',
+        name:'MySql',
+        img: require('./imgs/skills/mysql (1).png')
     },
 ];
