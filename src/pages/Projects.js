d1a450b9c3d9080518e12327c07dd7ad312f48f5
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-07-20 10:50:04 +0300

    add

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,7 +7,7 @@ const Projects = () => {
   const [obj]=useState(Info);
   const {mode}=useTheme();
   return (
-    <div id='projects' className={`  lg:pt-[15rem] md:pt-[5rem] pt-[5rem] z-41 h-full  overflow-hidden   ${mode?'':'bg-[#262626]'}`}>
+    <div id='projects' className={`  lg:pt-[22rem] md:pt-[5rem] pt-[5rem] z-41 h-full  overflow-hidden   ${mode?'':'bg-[#262626]'}`}>
      <My name={'Projects'} bg_name={'My'}/>
 <div className=''>
 {
